d44f04b2281127da82f1b4385018a055978831a0
gcclll <gccll.love@gmail.com>
2022-06-08 17:05:40 +0800

    2022-06-08 17:05:40

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(doom-themes flycheck ivy))
+ '(safe-local-variable-values '((after-save-hook . gcl/org-html-export-to-html)))
+ '(tramp-verbose 0))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(corfu-default ((t (:height 1.3)))))
