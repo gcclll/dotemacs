3669ddc6ad34e6477d921d54d34998dff3233a6c
gcclll <gccll.love@gmail.com>
2022-06-24 09:10:11 +0800

    2022-06-24 09:10:10

--- site-lisp/config/init-highlight.el
+++ site-lisp/config/init-highlight.el
@@ -8,7 +8,7 @@
 ;; (setq hl-paren-colors '("DarkOrange" "DeepSkyBlue" "DarkRed"))
 
 ;; highlight colors
-
+;; (highlight-indentation-mode)
 ;; (set-face-background 'highlight-indentation-face "#e3e3d3")
 ;; (set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
 
